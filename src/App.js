ec2494fff61b095582111679acddd802cbb60813
Will Thomas <wiillthomas@hotmail.com>
2019-03-03 22:21:19 +0000

    Fixed links

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 
 import "./App.css";
-/*
-Contendu
-Opcodo
-Between Borders
-Weather Mood
-Birth Year Films
-Calculator
-*/
+
 class Wrapper extends Component {
   constructor(props) {
     super(props);
@@ -19,6 +12,7 @@ class Wrapper extends Component {
         description:
           "a marketplace for content marketers to be paired with students. built a new front-end interface in HTML & CSS upon an existing wordpress theme",
         technology: "wordpress, html, php, js",
+
         hyperlink: "http://contendu.com"
       },
       2: {
@@ -26,6 +20,7 @@ class Wrapper extends Component {
         imgsrc: "test",
         description: "an agency website. build with wordpress & HTML",
         technology: "wordpress, html, css",
+
         hyperlink: "http://opcodo.com"
       },
       3: {
@@ -34,6 +29,7 @@ class Wrapper extends Component {
         description:
           "an online magazine celebrating diversity. took an design brief and built with wordpress & HTML.",
         technology: "wordpress, html, css",
+
         hyperlink: "http://between-borders.com/"
       },
       4: {
@@ -41,24 +37,24 @@ class Wrapper extends Component {
         imgsrc: "test",
         description: "an app that changes background color based on user input",
         technology: "js, html, css",
-        hyperlink: "http://google.com",
-        github: "https://wiillthomas.github.io/weather-mood/"
+        hyperlink: "https://wiillthomas.github.io/weather-mood/",
+        github: "https://github.com/wiillthomas/weather-mood"
       },
       5: {
         title: "birth year films",
         imgsrc: "test",
         description: "an app that shows most popular movies from a given year",
         technology: "react, html, css",
-        hyperlink: "http://google.com",
-        github: ""
+        hyperlink: "https://wiillthomas.github.io/birth-movies/",
+        github: "https://github.com/wiillthomas/birth-movies"
       },
       6: {
         title: "calculator",
         imgsrc: "test",
         description: "a simple calculator built in react",
         technology: "react, html, css",
-        hyperlink: "http://google.com",
-        github: ""
+        hyperlink: "https://wiillthomas.github.io/calculator/",
+        github: "https://github.com/wiillthomas/birth-movies"
       }
     };
   }
@@ -76,12 +72,14 @@ class Wrapper extends Component {
             description={this.state[1].description}
             technology={this.state[1].technology}
             hyperlink={this.state[1].hyperlink}
+            github={this.state[1].github}
           />
           <PorfolioItem
             title={this.state[2].title}
             imgsrc={this.state[2].imgsrc}
             description={this.state[2].description}
             technology={this.state[2].technology}
+            github={this.state[2].github}
             hyperlink={this.state[2].hyperlink}
           />
           <PorfolioItem
@@ -89,6 +87,7 @@ class Wrapper extends Component {
             imgsrc={this.state[3].imgsrc}
             description={this.state[3].description}
             technology={this.state[3].technology}
+            github={this.state[3].github}
             hyperlink={this.state[3].hyperlink}
           />
           <PorfolioItem
@@ -96,6 +95,7 @@ class Wrapper extends Component {
             imgsrc={this.state[4].imgsrc}
             description={this.state[4].description}
             technology={this.state[4].technology}
+            github={this.state[4].github}
             hyperlink={this.state[4].hyperlink}
           />
           <PorfolioItem
@@ -103,6 +103,7 @@ class Wrapper extends Component {
             imgsrc={this.state[5].imgsrc}
             description={this.state[5].description}
             technology={this.state[5].technology}
+            github={this.state[5].github}
             hyperlink={this.state[5].hyperlink}
           />
           <PorfolioItem
@@ -110,6 +111,7 @@ class Wrapper extends Component {
             imgsrc={this.state[6].imgsrc}
             description={this.state[6].description}
             technology={this.state[6].technology}
+            github={this.state[6].github}
             hyperlink={this.state[6].hyperlink}
           />
         </div>
@@ -124,6 +126,11 @@ class Wrapper extends Component {
 }
 
 class PorfolioItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="portfolio-item">
